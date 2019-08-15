634ff753c4d1d3fd21de07f91ed43ad30425bc1c
vlasiuk-andrii <31415926535pipipi>
2019-08-15 17:29:03 +0300

    Lazy WebElement initialization added

--- src/test/java/cucumber/pages/BasePage.java
+++ src/test/java/cucumber/pages/BasePage.java
@@ -1,7 +1,9 @@
 package cucumber.pages;
 
+import constants.Constants;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BasePage {
@@ -12,6 +14,12 @@ public class BasePage {
     public BasePage(WebDriver webDriver, WebDriverWait wait) {
         this.webDriver = webDriver;
         this.wait = wait;
-        PageFactory.initElements(webDriver, this);
+
+        //// Usual init:
+        // PageFactory.initElements(webDriver, this);
+        //// or
+        //// Here, when an operation is performed on an element the wait for its visibility starts from that moment only:
+        AjaxElementLocatorFactory factory = new AjaxElementLocatorFactory(webDriver, Constants.defaultTimeoutSeconds);
+        PageFactory.initElements(factory, this);
     }
 }
--- src/test/java/cucumber/pages/PaymentPage.java
+++ src/test/java/cucumber/pages/PaymentPage.java
@@ -1,6 +1,5 @@
 package cucumber.pages;
 
-import constants.Constants;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
